271cdc66cb880c5435bcd893249f1471e50f6104
Nathan Gardner <38595421+ngardner84@users.noreply.github.com>
2023-08-02 11:56:46 -0700

    Create setDock.sh

---
+++ setDock.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Reset the dock to its default state
+defaults write com.apple.dock persistent-apps -array
+
+# Add Finder to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/Finder.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Calendar to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/Calendar.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Firefox to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Firefox.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Google Chrome to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Microsoft Word to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Microsoft Word.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Microsoft PowerPoint to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Microsoft PowerPoint.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Adobe Illustrator to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Adobe Illustrator.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Adobe InDesign to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Adobe InDesign.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Adobe Photoshop to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Adobe Photoshop.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Terminal to the dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/Utilities/Terminal.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+# Add Downloads folder to the dock
+defaults write com.apple.dock persistent-others -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file:///Users/$USER/Downloads/</string><key>_CFURLStringType</key><integer>15</integer></dict></dict></dict>'
+
+# Add Trash to the dock
+defaults write com.apple.dock trash-full -bool false
+
+# Restart the dock
+killall Dock
