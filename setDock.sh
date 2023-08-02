671789049a38038f3a82dcbd7eed27c03b7ddc56
Nathan Gardner <38595421+ngardner84@users.noreply.github.com>
2023-08-02 13:47:00 -0700

    Update setDock.sh

--- setDock.sh
+++ setDock.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 # Set the dock items
-dockItems=(
+dockItems=(                                   
     "file:///System/Applications/Calendar.app"
     "file:///Applications/Firefox.app"
-    "file:///Applications/Google Chrome.app"
+    "file:///Applications/Google Chrome.app"                                 
     "file:///Applications/Microsoft Word.app"
     "file:///Applications/Microsoft PowerPoint.app"
-    "file:///Applications/Adobe Illustrator 2023.app"
-    "file:///Applications/Adobe InDesign 2023.app"
-    "file:///Applications/Adobe Photoshop 2023.app"
+    "file:///Applications/Adobe Illustrator 2023/Adobe Illustrator.app"
+    "file:///Applications/Adobe InDesign 2023/Adobe InDesign 2023.app"
+    "file:///Applications/Adobe Photoshop 2023/Adobe Photoshop 2023.app"
     "file:///System/Applications/Utilities/Terminal.app"
     "file:///Applications/Audacity.app"
 )
 
-sleep 1s
+defaults delete com.apple.dock persistent-apps
+defaults delete com.apple.dock persistent-others
 
 # Add the new dock items
 for item in "${dockItems[@]}"; do
     defaults write com.apple.dock persistent-others -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$item</string><key>_CFURLStringType</key><integer>15</integer></dict></dict></dict>"
 done
-
-sleep 1s
 
 # Restart the dock
 killall Dock
